6ae9e7b71e49989bc6ffa7bc718e91295971f051
gobarcanin <gobarcanin@gmail.com>
2020-06-06 23:16:11 +0200

    [Project] Create inital project setup

---
+++ mArtian/mArtian/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  mArtian
+//
+//  Created by Goran Obarcanin on 06/06/2020.
+//  Copyright © 2020 MartianAndMAchine. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
