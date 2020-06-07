c1de1e55ee76dbdb569e0624af60bd992674b777
gobarcanin <gobarcanin@gmail.com>
2020-06-07 15:15:28 +0200

    [Api][Service] Added empty files for service layer

---
+++ mArtian/mArtian/API/Service/Implementation/DefaultCommentsService.swift
@@ -0,0 +1,9 @@
+//
+//  DefaultCommentsService.swift
+//  mArtian
+//
+//  Created by Goran Obarcanin on 07/06/2020.
+//  Copyright © 2020 MartianAndMAchine. All rights reserved.
+//
+
+import Foundation
