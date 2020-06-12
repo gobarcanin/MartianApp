0bdf5c56668f6738dec3cacf0a2369f11b530fda
gobarcanin <gobarcanin@gmail.com>
2020-06-12 20:43:00 +0200

    [API][Model] DefaultPost and Mapped post now conform to Post protocol

--- mArtian/mArtian/API/Models/Post/Post.swift
+++ mArtian/mArtian/API/Models/Post/Post.swift
@@ -8,9 +8,32 @@
 
 import Foundation
 
-struct Post: ApiParameter {
+protocol Post: ApiParameter {
+    var userId: Int { get }
+    var id: Int { get }
+    var title: String { get }
+    var body: String { get }
+}
+
+struct DefaultPost: Post {
     let userId: Int
     let id: Int
     let title: String
     let body: String
+}
+
+struct MappedPost: Post {
+    typealias Post = DefaultPost
+    var userId: Int
+    var id: Int
+    var title: String
+    var body: String
+    var user: User?
+    init(with default: Post, user: User?) {
+        self.userId = `default`.userId
+        self.id = `default`.id
+        self.title = `default`.title
+        self.body = `default`.body
+        self.user = user
+    }
 }
